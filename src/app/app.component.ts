e5007b8540158fe6dbc7e4e2a19ed1d73ec5086b
Artem Sizonenko <admondantes1@gmail.com>
2017-07-18 19:14:08 +0300

    read more

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,109 +1,121 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['../../node_modules/normalize.css/normalize.css',
+        './app.component.css']
 })
 export class AppComponent {
-  public leftBasket: any = [{
-    src: "assets/images/1.jpg",
-    name: 'Siku - 1:87 Scale - Low Loader With John ...',
-    price: '$19.55'
-  },
-    {
-      src: "assets/images/2.jpg",
-      name: 'Siku - 1:87 Scale - Low Loader With John ...',
-      price: '$19.55'
+    public leftBasket: any = [{
+        src: "assets/images/1.jpg",
+        name: 'Siku - 1:87 Scale - Low Loader With John ...',
+        price: '$19.55'
     },
-    {
-      src: "assets/images/3.jpg",
-      name: 'Siku - 1:87 Scale - Low Loader With John ...',
-      price: '$19.55'
-    },
-    {
-      src: "assets/images/4.jpg",
-      name: 'Siku - 1:87 Scale - Low Loader With John ...',
-      price: '$19.55'
-    },
+        {
+            src: "assets/images/2.jpg",
+            name: 'Siku - 1:87 Scale - Low Loader With John ...',
+            price: '$19.55'
+        },
+        {
+            src: "assets/images/3.jpg",
+            name: 'Siku - 1:87 Scale - Low Loader With John ...',
+            price: '$19.55'
+        },
+        {
+            src: "assets/images/4.jpg",
+            name: 'Siku - 1:87 Scale - Low Loader With John ...',
+            price: '$19.55'
+        },
 
-    {
-      src: "assets/images/5.jpg",
-      name: 'Siku - 1:87 Scale - Low Loader With John ...',
-      price: '$19.55'
-    },
-    {
-      src: "assets/images/6.jpg",
-      name: 'Siku - 1:87 Scale - Low Loader With John ...',
-      price: '$19.55'
-    },
-    {
-      src: "assets/images/7.jpg",
-      name: 'Siku - 1:87 Scale - Low Loader With John ...',
-      price: '$19.55'
-    },
-    {
-      src: "assets/images/8.jpg",
-      name: 'Siku - 1:87 Scale - Low Loader With John ...',
-      price: '$19.55'
-    },
+        {
+            src: "assets/images/5.jpg",
+            name: 'Siku - 1:87 Scale - Low Loader With John ...',
+            price: '$19.55'
+        },
+        {
+            src: "assets/images/6.jpg",
+            name: 'Siku - 1:87 Scale - Low Loader With John ...',
+            price: '$19.55'
+        },
+        {
+            src: "assets/images/7.jpg",
+            name: 'Siku - 1:87 Scale - Low Loader With John ...',
+            price: '$19.55'
+        },
+        {
+            src: "assets/images/8.jpg",
+            name: 'Siku - 1:87 Scale - Low Loader With John ...',
+            price: '$19.55'
+        },
 
-    {
-      src: "assets/images/9.jpg",
-      name: 'Siku - 1:87 Scale - Low Loader With John ...',
-      price: '$19.55'
-    },
-    {
-      src: "assets/images/1.jpg",
-      name: 'Siku - 1:87 Scale - Low Loader With John ...',
-      price: '$19.55'
-    },
-    {
-      src: "assets/images/2.jpg",
-      name: 'Siku - 1:87 Scale - Low Loader With John ...',
-      price: '$19.55'
-    },
-    {
-      src: "assets/images/3.jpg",
-      name: 'Siku - 1:87 Scale - Low Loader With John ...',
-      price: '$19.55'
+        {
+            src: "assets/images/9.jpg",
+            name: 'Siku - 1:87 Scale - Low Loader With John ...',
+            price: '$19.55'
+        },
+        {
+            src: "assets/images/1.jpg",
+            name: 'Siku - 1:87 Scale - Low Loader With John ...',
+            price: '$19.55'
+        },
+        {
+            src: "assets/images/2.jpg",
+            name: 'Siku - 1:87 Scale - Low Loader With John ...',
+            price: '$19.55'
+        },
+        {
+            src: "assets/images/3.jpg",
+            name: 'Siku - 1:87 Scale - Low Loader With John ...',
+            price: '$19.55'
+        }
+    ];
+
+    public rightBasket: any = [];
+
+    create(index): any {
+        event.preventDefault();
+        this.rightBasket.push(index);
     }
-  ];
 
-  public rightBasket: any = [];
-
-  create(index): any {
-    event.preventDefault();
-    this.rightBasket.push(index);
-  }
-
-  openMenu(index) {
-    let numberImage = this.rightBasket.indexOf(index);
-    for (let i = 0; i < this.rightBasket.length; i++) {
-      if (numberImage === i) {
-        document.querySelectorAll('div.note-content-menu')[i].classList.toggle('open-menu');
-      }
+    openMenu(index) {
+        let numberImage = this.rightBasket.indexOf(index);
+        for (let i = 0; i < this.rightBasket.length; i++) {
+            if (numberImage === i) {
+                document.querySelectorAll('div.note-content-menu')[i].classList.toggle('open-menu');
+            }
+        }
     }
-  }
 
-  openList(index) {
-    event.preventDefault();
-    let numberImage = this.rightBasket.indexOf(index);
-    for (let i = 0; i < this.rightBasket.length; i++) {
-      if (numberImage === i) {
-        document.querySelectorAll('div.menu_options')[i].classList.toggle('open');
-      }
+    openList(index) {
+        event.preventDefault();
+        let numberImage = this.rightBasket.indexOf(index);
+        for (let i = 0; i < this.rightBasket.length; i++) {
+            if (numberImage === i) {
+                document.querySelectorAll('div.menu_options')[i].classList.toggle('open');
+            }
+        }
     }
-  }
 
-  removeProduct(index) {
-    event.preventDefault();
-    let numberImage = this.rightBasket.indexOf(index);
-    for (let i = 0; i < this.rightBasket.length; i++) {
-      if (numberImage === i) {
-        this.rightBasket.splice(i, 1);
-      }
+    removeProduct(index) {
+        event.preventDefault();
+        let numberImage = this.rightBasket.indexOf(index);
+        for (let i = 0; i < this.rightBasket.length; i++) {
+            if (numberImage === i) {
+                this.rightBasket.splice(i, 1);
+            }
+        }
     }
-  }
+
+    openPaper() {
+        event.preventDefault();
+        // let text = document.getElementById('paper').textContent;
+        // document.getElementById('paper').textContent = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' +
+        //     ' Accusantium ad amet architecto atque commodi cum cupiditate dignissimos doloribus enim et eveniet, iste' +
+        //     ' iure magni, minus modi nisi nobis porro, quod tenetur voluptatum. A aliquam aspernatur atque, consequatur' +
+        //     ' deserunt dolorum ea eaque earum et excepturi facere incidunt itaque labore laudantium magnam natus'
+
+        document.querySelector('.note-top-text-wrapper').classList.toggle('open');
+    }
 }
 
